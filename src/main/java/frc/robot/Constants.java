343ec41e4aaf0af3639c03cbb776da9dd2c9c577
VillarealKaleb <villakal20@muhs.edu>
2024-01-13 11:49:47 -0600

    Initial commit of Intake subsystem

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Constants {
+    //---------------------CAN IDs---------------------\\
+    public static final int INTAKE_MOTOR_CAN_ID = 1;
+    public static final int INTAKE_SECOND_MOTOR_CAN_ID = 2;
+   
+    //-----------------------INVERTED-----------------------\\
+    public static final boolean INTAKE_MOTOR_L_INVERTED = false;
+    public static final boolean INTAKE_MOTOR_R_INVERTED = false;
+
+
+   
+
+}
