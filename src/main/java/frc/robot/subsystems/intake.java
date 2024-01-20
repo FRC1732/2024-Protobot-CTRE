188996edab27fe1a076bfd647c935f69c167cd71
Katie Chandek <katiechandek@gmail.com>
2024-01-20 14:56:58 -0600

    fixed intake.java issues

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -5,10 +5,8 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.hal.CANAPITypes.CANDeviceType;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.Constants;
 
 public class Intake{
@@ -24,7 +22,7 @@ public class Intake{
     public static Double intakeMotorRSpeed = 0.50;
 
     
-    public intake() {
+    public Intake() {
         intakeLCanSparkMax = new CANSparkMax(Constants.INTAKE_MOTOR_CAN_ID,  CANSparkMax.MotorType.kBrushless);
         intakeRCanSparkMax = new CANSparkMax(Constants.INTAKE_SECOND_MOTOR_CAN_ID, CANSparkMax.MotorType.kBrushless);
 
@@ -72,20 +70,21 @@ public class Intake{
 
     }
 
-        double vel = Double.MIN_VALUE;
+        private double getAcceleration(){
+        double prev_vel = Double.MIN_VALUE;
         double prev_time = Double.MIN_VALUE;
-        private double getAcceleration();
-        double current_vel = shooterMotorHigh.getEncoder().getVelocity();
+
+        double current_vel = intakeRCanSparkMax.getEncoder().getVelocity();
         double acc = 0;
-        if(prev vel == Double.MIN_VALUE){
+        if(prev_vel == Double.MIN_VALUE){
             prev_vel=current_vel;
         }
         else {
-            acc = (current_vel-prev_vel)/(RobotController.getFGPATime()/60000000.0 - prev_time);   
+             acc = (current_vel-prev_vel)/(RobotController.getFPGATime()/60000000.0 - prev_time);   
         }
         
         prev_vel = current_vel;
         prev_time = RobotController.getFPGATime()/60000000.0;
         return acc;
-    
+        }  
 }
