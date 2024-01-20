566836b5f74c9a78cf8f0b878a6d5e76aec424bc
Yousef Abusamhadana <abusay6@muhs.edu>
2024-01-20 13:45:31 -0600

    shuffleboard work

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,7 +5,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,12 +17,15 @@ public class Shooter extends SubsystemBase {
   private CANSparkMax shooterMotorHigh;
   private CANSparkMax shooterMotorLow;
   private CANSparkMax shooterMotorTilt;
+  private GenericEntry speedEntryHigh;
+  private GenericEntry speedEntryLow;
+  // private GenericEntry speedEntryTilt;
   
   /** Creates a new Shooter. */
 
  
   public static Double SHOOTER_MOTOR_HIGH_SPEED = 0.50;
-  public static Double SHOOTER_MOTOR_LOW_SPEED = -0.50;
+  public static Double SHOOTER_MOTOR_LOW_SPEED = 0.50;
   public static Double SHOOTER_MOTOR_TILT_SPEED = 0.50;
 
   public Shooter() {
@@ -52,12 +57,37 @@ public class Shooter extends SubsystemBase {
     shooterMotors.addDouble("LowVelocity", () -> shooterMotorLow.getEncoder().getVelocity());
     shooterMotors.addDouble("TiltVelocity", () -> shooterMotorTilt.getEncoder().getVelocity());
     shooterMotors.addDouble("TiltAngle", () -> shooterMotorTilt.getEncoder().getPosition());
-    //shooterMotors.addDouble("HighAcceleration", () -> shooterMotorHigh.
+    shooterMotors.addDouble("HighAcceleration", () -> getAcceleration());
+    shooterMotors.addDouble("LowAcceleration", () -> getAcceleration());
 
+    speedEntryHigh = shooterMotors.add("MotorHighSpeed", SHOOTER_MOTOR_HIGH_SPEED ).getEntry();
+    speedEntryLow = shooterMotors.add("MotorLowSpeed", SHOOTER_MOTOR_LOW_SPEED ).getEntry();
+    //speedEntryTilt = shooterMotors.add("MotorTiltSpeed", SHOOTER_MOTOR_TILT_SPEED ).getEntry(); 
   }
-  
+
+  double prev_vel = Double.MIN_VALUE;
+  double prev_time = Double.MIN_VALUE;
+  private double getAcceleration() {
+      double current_vel = shooterMotorHigh.getEncoder().getVelocity();
+      double acc = 0;
+      if (prev_vel == Double.MIN_VALUE) {
+        prev_vel = current_vel;
+      } else {
+        acc = (current_vel - prev_vel)/(RobotController.getFPGATime()/ 60000000.0 - prev_time);
+
+      } 
+      prev_vel = current_vel;
+      prev_time = RobotController.getFPGATime() / 60000000.0;
+      return acc;
+
+      }
+    
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SHOOTER_MOTOR_HIGH_SPEED = speedEntryHigh.getDouble(0);
+    SHOOTER_MOTOR_LOW_SPEED = speedEntryLow.getDouble(0);
+    // SHOOTER_MOTOR_TILT_SPEED = speedEntryTilt.getDouble(0);
   }
 }
