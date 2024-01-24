e165c5e40efa17de3132cdb8724b5ba057816e8d
VillarealKaleb <villakal20@muhs.edu>
2024-01-23 20:40:39 -0600

    CAN ID's updated from Electrical spreadsheet

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,12 +6,26 @@ package frc.robot;
 
 /** Add your docs here. */
 public class Constants {
+                    //MOTOR IDs\\
     public static final int SHOOTER_MOTOR_HIGH_CAN_ID = 61;
     public static final int SHOOTER_MOTOR_LOW_CAN_ID = 62;
-    public static final int SHOOTER_MOTOR_TILT_CAN_ID = 1;
-    public static final int FEEDER_MOTOR_LEFT_CAN_ID = 2;
-    public static final int FEEDER_MOTOR_RIGHT_CAN_ID = 3;
-
+    public static final int SHOOTER_MOTOR_TILT_CAN_ID = 3;
+    public static final int FEEDER_MOTOR_LEFT_CAN_ID = 4;
+    public static final int FEEDER_MOTOR_RIGHT_CAN_ID = 5;
+    public static final int FL_DRIVE_MOTOR_CAN_ID = 30;
+    public static final int FL_TURN_MOTOR_CAN_ID = 31;
+    public static final int FR_DRIVE_MOTOR_ID = 40;
+    public static final int FR_TURN_MOTOR_ID = 41;
+    public static final int BL_DRIVE_MOTOR_ID = 10;
+    public static final int BL_TURN_MOTOR_ID = 11;
+    public static final int BR_DRIVE_MOTOR_ID = 20;
+    public static final int BR_TURN_MOTOR_ID = 21;
+                    //CANCODERS\\
+    public static final int CANCODER_FL = 13;
+    public static final int CANCODER_FR = 11;
+    public static final int CANCODER_BL = 17;
+    public static final int CANCODER_BR = 15;;
+                    //INVERTED\\
     public static final boolean SHOOTER_MOTOR_HIGH_INVERTED = false;
     public static final boolean SHOOTER_MOTOR_LOW_INVERTED = false;
     public static final boolean SHOOTER_MOTOR_TILT_INVERTED = false;
